45090d40e86f7bc950ca1d08b662faedbee5bc04
Luyuan Xing <lxing.nyc@gmail.com>
2016-12-03 15:31:39 -0500

    updated routes to only have create and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :numbers
-  root 'numbers#new'
+  # resources :numbers
+  root 'numbers#create'
+  post '/numbers/' => 'numbers#create'
+  delete '/numbers/:id' => 'numbers#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
