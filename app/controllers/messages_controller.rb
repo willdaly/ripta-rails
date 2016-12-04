4a1d04471e6a848e907186a15c137e88b4e4f639
Robert Daly <rwilliamdaly@Roberts-MacBook-Pro.local>
2016-12-04 14:14:41 -0500

    updated reply on messages#reply

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
   def reply
     @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
     body = DataParser.new(params["Body"]).getRouteId
-    reply = "user stop id: #{body}"
+    reply = "#{body}"
     sms = @client.messages.create(
       from: TWILIO_NUMBER,
       to: params["From"],
