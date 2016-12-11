5cac0fca0432a78fcdd00fb3f1c91279f0b95c5c
Robert Daly <rwilliamdaly@Roberts-MacBook-Pro.local>
2016-12-11 13:31:44 -0500

    method to create vehicle objects

--- app/services/vehicle_positions_service.rb
+++ app/services/vehicle_positions_service.rb
@@ -1,13 +1,13 @@
 class VehiclePositionsService
   attr_reader :response
-  
+
   def initialize
     url = HTTParty.get("http://realtime.ripta.com:81/api/vehiclepositions?format=json")
     @response = JSON.parse(url.body)
   end
 
-  def trip_ids
-    entities.map{|entity| trip_id(entity) }
+  def new_vehicles
+    entities.map{|entity| new_vehicle(entity) }
   end
 
   private
@@ -18,6 +18,12 @@ class VehiclePositionsService
 
   def trip_id entity
     entity["vehicle"]["trip"]["trip_id"].to_i
+  end
+
+  def new_vehicle entity
+    vehicle = entity["vehicle"]
+    trip = vehicle["trip"]
+    Vehicle.new(id: trip["vehicle"]["id"], trip_id: trip["trip_id"], stop_id: vehicle["stop_id"], route_id: trip["route_id"])
   end
 
 end
