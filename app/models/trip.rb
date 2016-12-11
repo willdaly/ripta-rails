53a48ec534eb0b7e5b2cc9c736327bc971630fdc
Robert Daly <rwilliamdaly@Roberts-MacBook-Pro.local>
2016-12-11 11:04:18 -0500

    get the index number of a stop on a trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,9 @@ class Trip < ActiveRecord::Base
   belongs_to :route
   has_many :route_stops, through: :route
   has_many :stops, -> { includes(:route_stops).order('route_stops.order asc')}, through: :route_stops
+
+  def stop_index stop
+    stops.index(stop)
+  end
+
 end
